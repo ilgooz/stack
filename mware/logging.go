c3cbb0aed7d122bbf59650e763851bdc7c9c30f1
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2015-10-05 07:56:49 +0300

    identify each request in logs

--- mware/logging.go
+++ mware/logging.go
@@ -1,18 +1,23 @@
 package mware
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/codegangsta/negroni"
+	"github.com/ilgooz/cryptoutils"
 )
 
 func Logging(h http.Handler) http.Handler {
-	l := &negroni.Logger{log.New(os.Stdout, "[api] ", 0)}
-
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		nw := negroni.NewResponseWriter(w)
+
+		l := &negroni.Logger{
+			log.New(os.Stdout, fmt.Sprintf("[api] request id: %s ", cryptoutils.RandToken(5)), 0),
+		}
+
 		l.ServeHTTP(nw, r, h.ServeHTTP)
 	})
 }
